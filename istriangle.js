28f8d4da9da7e6d46328038e79563da1dd1dc74a
Sudarshana Tamuly <47175592+sudarshana12@users.noreply.github.com>
2021-10-27 15:23:56 +0530

    updated

--- istriangle.js
+++ istriangle.js
@@ -4,13 +4,20 @@ const outputEl = document.querySelector('#output');
 
 function calculateSumOfTriangles(angle1, angle2, angle3){
     const sumOfTriangles = angle1 + angle2 + angle3;
+    // console.log(sumOfTriangles);
     return sumOfTriangles;
-}
+    
+ }
 
 
 function isTriangle(){
-    const sumOfTriangles = calculateSumOfTriangles(60,60,60);
-    console.log(sumOfTriangles);
+    const sumOfTriangles = calculateSumOfTriangles(Number(inputs[0].value), Number(inputs[1].value), Number(inputs[2].value));
+    if(sumOfTriangles === 180){
+        outputEl.innerText = "Yay, the angles form a triangle";
+    }else{
+        outputEl.innerText = "Nay, the angles don't form a triangle";
+    }
+    
 
 }
     
