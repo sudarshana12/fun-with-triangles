23c02636ad62a40e878c6009851dc7fe0388e141
Sudarshana Tamuly <47175592+sudarshana12@users.noreply.github.com>
2021-10-27 23:06:35 +0530

    updated

---
+++ quiz.js
@@ -0,0 +1,11 @@
+const quizForm = document.querySelector(".quiz-form");
+const submitAnswerBtn = document.querySelector("#submit-answer-btn");
+const outputEl = document.querySelector("#output");
+
+const correctAnswers = ["90°", "right angled"];
+
+function calculateScore(){
+
+}
+
+submitAnswerBtn.addEventListener('click', calculateScore);
